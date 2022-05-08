db0a3af95ade8850c3996999b7862332f07d8dac
worlddeleteRin <worlddelete0@gmail.com>
2022-05-08 23:00:17 +0300

    add new snippets (refactor require)

--- lua/main3.lua
+++ lua/main3.lua
@@ -8,13 +8,49 @@ local c = ls.choice_node
 ls.add_snippets(nil, {
   all = {
     s(
+      "clg",
+      fmt(
+        [[
+          console.log('tst {1}')
+        ]],
+        {
+          i(1, "")
+        }
+      )
+    ),
+    s(
+      "div",
+      fmt(
+        [[
+          <div>
+            {1}
+          </div>
+        ]],
+        {
+          i(1, "")
+        }
+      )
+    ),
+    s(
+      "divc",
+      fmt(
+        [[
+          <div class="{1}">
+          </div>
+        ]],
+        {
+          i(1, "")
+        }
+      )
+    ),
+    s(
       "cc",
       fmt(
         [[
-          {}
+          <!-- {1} -->
         ]],
         {
-            t "# "
+          i(1, "")
         }
       )
     ),
@@ -25,6 +61,28 @@ ls.add_snippets(nil, {
       fmt(
         [[
           console.log('tst {1}')
+        ]],
+        {
+          i(1, "")
+        }
+      )
+    ),
+  },
+  vue = {
+    s(
+      "bgr",
+      fmt(
+        [[
+          background-color: red;
+        ]],
+        {}
+      )
+    ),
+    s(
+      "bg",
+      fmt(
+        [[
+          background-color: {1};
         ]],
         {
           i(1, "")
