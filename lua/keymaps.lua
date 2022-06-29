57744bfe66b88f2732d4a966a9eede89fb7d4a0c
worlddeleteRin <worlddelete0@gmail.com>
2022-06-29 15:06:08 +0300

    ✨add keymaps to move cursor in insert mode

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,13 +1,14 @@
-vim.api.nvim_set_keymap(
-  'n', '<M-h>', '<cmd>wincmd h<CR>', {silent= true}
-)
-vim.api.nvim_set_keymap(
-  'n', '<M-l>', '<cmd>wincmd l<CR>', {silent= true}
-)
-vim.api.nvim_set_keymap(
-  'n', '<M-j>', '<cmd>wincmd j<CR>', {silent= true}
-)
-vim.api.nvim_set_keymap(
-  'n', '<M-k>', '<cmd>wincmd k<CR>', {silent= true}
-)
+-- move windowm maps
+vim.api.nvim_set_keymap( 'n', '<M-h>', '<cmd>wincmd h<CR>', {silent= true})
+vim.api.nvim_set_keymap( 'n', '<M-l>', '<cmd>wincmd l<CR>', {silent= true})
+vim.api.nvim_set_keymap( 'n', '<M-j>', '<cmd>wincmd j<CR>', {silent= true})
+vim.api.nvim_set_keymap( 'n', '<M-k>', '<cmd>wincmd k<CR>', {silent= true})
+
+-- move cursor in insert mode
+vim.api.nvim_set_keymap('i', '<C-h>', '<Left>', {silent=true})
+vim.api.nvim_set_keymap('i', '<C-l>', '<Right>', {silent=true})
+vim.api.nvim_set_keymap('i', '<C-j>', '<Down>', {silent=true})
+vim.api.nvim_set_keymap('i', '<C-k>', '<Up>', {silent=true})
+
+-- call local rename var function
 vim.api.nvim_set_keymap('n', 'rn', '<cmd>lua RenameVar()<CR>', {})
