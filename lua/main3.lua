bc1325b11cb769ba32cabc2a3c4b5afe67d2cd25
worlddeleteRin <worlddelete0@gmail.com>
2022-07-06 00:19:47 +0300

    add new snippets

--- lua/main3.lua
+++ lua/main3.lua
@@ -8,6 +8,16 @@ local c = ls.choice_node
 ls.add_snippets(nil, {
   all = {
     s(
+      "typo",
+      fmt(
+        [[
+          ✏️
+        ]],
+        {
+        }
+      )
+    ),
+    s(
       "cons",
       fmt(
         [[
